cb003d65d651ffa7b95d1a621fc785fc3db974a7
Felipe Tavares <90937722+FelipeLTavares@users.noreply.github.com>
2023-03-15 13:39:17 -0300

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
+require('pg');
 
 const database = new Sequelize(process.env.DB_CONNECTION_URL, {
   dialect: process.env.DB_DIALECT,
